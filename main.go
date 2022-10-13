250b4fff1655c94f6aa0e80e4a2b37f1f3ec5363
gopherchai <baoyangchai@gmail.com>
2022-10-13 12:17:59 +0800

    update

--- main.go
+++ main.go
@@ -1,24 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"net"
-)
+import "fmt"
 
-func main() {
-	ss := "abcd"
-	fmt.Println(ss)
-	s := make(map[int]string, 1)
-	for i := 0; i < 10; i++ {
-		s[i] = fmt.Sprintf("%d", i)
-	}
-	l, err := net.Listen("tcp", "")
-	if err != nil {
-		return
-	}
-	for {
-		l.Accept()
-
-	}
+func demo(user int) int {
+	var admin int
+	admin = user
+	return admin
+}
+func void() {
 
 }
+func main() {
+	var user int
+	void()
+	fmt.Println(user, demo(0))
+
+}
+
+// go run -gcflags "-m -l" main.go
+// # command-line-arguments
+// ./main.go:16:13: ... argument does not escape
+// ./main.go:16:13: user escapes to heap
+// ./main.go:16:24: demo(0) escapes to heap
+// 0 0
+
+/*
+如果一个函数返回对一个变量的引用，那么他会发生逃逸
+任何时候 一个值被分享到函数栈帧范围外，他都会在堆上被重新分配
+如果函数return之后，确定变量不再被引用，则将其分配到栈上
+如果编译器不能确保变量在函数return之后不在被引用，则变量被分配到堆上
+如果一个局部变量非常大，那么它也应该被分配到堆上，而不是栈上
+*/
